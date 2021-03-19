34a4f66606763b701f6d1fa5fba9fdf8eaee881f
hellogithub1234567890 <80927519+hellogithub1234567890@users.noreply.github.com>
2021-03-20 03:40:08 +0530

    Update nodejs.js

--- nodejs.js
+++ nodejs.js
@@ -1,6 +1,11 @@
 var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World node!');
-}).listen(8080);
+ 
+var server = http.createServer(function handler(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello World!!!');
+});
+ 
+server.listen(9000);
+console.log('Server running at http://127.0.0.1:9000/');
