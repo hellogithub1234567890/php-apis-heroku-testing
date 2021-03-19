ae436df223726e4644d716b0b5cd68f1fe7085bc
hellogithub1234567890 <80927519+hellogithub1234567890@users.noreply.github.com>
2021-03-19 14:31:22 +0530

    Update index.php
    edited

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "eu-az-sql-serv1.database.windows.net";
+$servername = "eu-az-sql-servca0b8bea36444442ae8d0b9d8df984c5.database.windows.net";
 $username = "u9rae5khdldm5mi";
 $password = "O%EVD2h0Bn@dPSrb7IrM%Df3Q";
 $dbname = "dx1tqvr61l3y3ym";
